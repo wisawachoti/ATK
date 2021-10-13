e22b31c4fda8a7187df917048f6070b8744e5159
wisawachoti <guypeach@hotmail.co.th>
2021-10-13 17:13:23 +0700

    Create ai_model.php

---
+++ models/ai_model.php
@@ -0,0 +1,28 @@
+<?php
+
+class aiModel{
+
+
+    public function __construct (){
+
+    }
+
+    public static function getid($key,$key_id){
+       
+        require("connection_connect.php");
+        $sql = "SELECT MAX('$key_id') as id FROM '$key'";
+        $result = $conn->query($sql);
+        $my_row = $result->fetch_assoc();
+
+        $id = $my_row[id];
+
+        require("connection_close.php");
+
+        return $id;
+    }
+
+}
+
+
+
+?>
