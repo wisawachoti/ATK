e0a793ac2b8e29410f9c07c0d958007764660753
MIIKI <nakrob.s@ku.th>
2021-11-07 02:15:08 +0700

    Update staffcheckpoint_controller.php

--- controllers/staffcheckpoint_controller.php
+++ controllers/staffcheckpoint_controller.php
@@ -5,5 +5,21 @@ class StaffCheckpointController{
         $staffcheckpointList=StaffCheckpoint::getAll();
         require_once('views/staffcheckpoint/index_StaffCheckpoint.php');
     }
+
+    public function newRate()
+    {
+        $productList = StaffCheckpoint::getAll();
+        require_once('views/staffcheckpoint/newStaffCheckpoint.php');
+    }
+
+    public function addStaffCheckpoint()
+    {
+        $staffid = $_GET['Product_ID'];
+        $staffname = $_GET['Quantity'];
+
+        StaffCheckpoint::Add($staffid,$staffname);
+        StaffCheckpointController::index();
+
+    }
 }
 ?>
