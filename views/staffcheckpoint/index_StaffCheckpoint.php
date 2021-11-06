622cf584709bfad7c8af50e18f8ae4d68e64cbab
MIIKI <nakrob.s@ku.th>
2021-11-07 01:51:42 +0700

    Update index_StaffCheckpoint.php

--- views/staffcheckpoint/index_StaffCheckpoint.php
+++ views/staffcheckpoint/index_StaffCheckpoint.php
@@ -3,13 +3,21 @@
 <td>ชื่อเจ้าหน้าที่</td>
 <td>Update</td>
 <td>Delete</td>
-
+New StaffCheckpoint [<a href="?controller=staffcheckpoint&action=newStaffCheckpoint">click</a>]<br>
+<form method="get" action="">
+    <input type="text" name="key">
+    <input type="hidden" name="controller" value="staffcheckpoint"/>
+    <button type="submit" name="action" value="searchStaffCheckpoint">Search</button>
+</form>
 <?php
     foreach($staffcheckpointList as $staffcheckpoint)
     {
         echo"
         <tr><td>$staffcheckpoint->staffid</td>
         <td>$staffcheckpoint->staffname</td>
+
+        <td><a href=?controller=staffcheckpoint&action=updateFormStaffCheckpoint&staff_c_id=$staffcheckpoint->staffid&name=$staffcheckpoint->staffname>update</td>
+        <td><a href=?controller=staffcheckpoint&action=deleteConfirmStaffCheckpoint&staff_c_id=$staffcheckpoint->staffid&name=$staffcheckpoint->staffname>delete</td>
         ";
     }
     echo "</table>";
