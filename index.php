51c48e3716e8953f17ed1ebd41c5a086af867832
MIIKI <nakrob.s@ku.th>
2021-11-06 23:58:12 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    if(isset($_GET['controller'])&&isset($_GET['action']))
+    {
+        $controller=$_GET['controller'];
+        $action=$_GET['action'];
+    }
+    else
+    {
+        $controller='pages';
+        $action='home';
+    }
+?>
+<html>
+    <head></head>
+    <body>
+        <?php echo "controller=".$controller," action=".$action ; ?>
+        <br>[<a href="?controller=pages&action=home">Home</a>]
+            [<a href="?controller=bill&action=index">StaffCheckpoint</a>]
+            [<a href="?controller=billdetail&action=index">WorkSchedule</a>]
+        <br>
+        <?php require_once("routes.php"); ?>
+    </body>
+</html>
