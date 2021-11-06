981291ca6526745335e8210de6446f7979be48ae
MIIKI <nakrob.s@ku.th>
2021-11-07 00:00:23 +0700

    Create routes.php

---
+++ routes.php
@@ -0,0 +1,31 @@
+<?php
+    $controllers =array('pages'=>['home','error']);
+
+    function call($controller,$action)
+    {
+        require_once("controllers/".$controller."_controller.php");
+        switch($controller)
+        {
+            case "pages":$controller= new PagesController();
+                        break;
+
+        }
+        $controller->{$action}();
+    }
+
+    if(array_key_exists($controller,$controllers))
+    {
+        if(in_array($action,$controllers[$controller]))
+        {
+            call($controller,$action);
+        }
+        else
+        {
+            call('pages','error');
+        }
+    }
+    else
+    {
+        call('pages','error');
+    }
+?>
