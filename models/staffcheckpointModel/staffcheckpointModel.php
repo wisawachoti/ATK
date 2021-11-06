dcd454f7a3913eb86f98450e545d87c4cf2d1f2a
MIIKI <nakrob.s@ku.th>
2021-11-07 03:14:47 +0700

    Update staffcheckpointModel.php

--- models/staffcheckpointModel/staffcheckpointModel.php
+++ models/staffcheckpointModel/staffcheckpointModel.php
@@ -31,5 +31,25 @@ class StaffCheckpoint{
         require("connection_close.php");
         return "add success $result rows";
     }
+
+        public static function search($key)
+    {
+        $staffcheckpointList=[];
+        require("connection_connect.php");
+        $sql="SELECT * FROM StaffCheckpoint
+        WHERE(StaffCheckpoint.staff_c_id like '%$key%' or StaffCheckpoint.name like '%$key%')
+        ORDER BY StaffCheckpoint.staff_c_id";
+        $result=$conn->query($sql);
+        while($my_row=$result->fetch_assoc())
+        {
+            $staffid=$my_row[staff_c_id];
+            $staffname=$my_row[name];
+
+            $staffcheckpointList[]=new StaffCheckpoint($staffid,$staffname);
+        }
+        require("connection_close.php");
+        return $staffcheckpointList;
+
+    }
 }
 ?>
