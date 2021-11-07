bb0c9c6c8cf2e94900976dad3f0f6aeb17bb4706
MIIKI <nakrob.s@ku.th>
2021-11-07 20:53:05 +0700

    Create staffcheckpointModel.php

---
+++ models/workscheduleModel/staffcheckpointModel.php
@@ -0,0 +1,26 @@
+<?php
+class StaffCheckpoint{
+    public $staffid , $staffname;
+    public function __construct($staffid , $staffname)
+    {
+        $this->staffid = $staffid;
+        $this->staffname = $staffname;
+    }
+    public static function getAll()
+    {
+        $staffcheckpointList=[];
+        require("connection_connect.php");
+        $sql="SELECT staff_c_id , cname FROM StaffCheckpoint
+        ORDER BY staff_c_id";
+        $result =$conn->query($sql);
+        while($my_row=$result->fetch_assoc())
+        {
+            $staffid=$my_row[staff_c_id];
+            $staffname=$my_row[cname];
+            $staffcheckpointList[]=new StaffCheckpoint($staffid,$staffname);
+        }
+        require("connection_close.php");
+        return $staffcheckpointList;
+    }
+}
+?>
