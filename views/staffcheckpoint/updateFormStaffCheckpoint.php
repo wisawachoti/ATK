9d9fb8e8c8ec680edf5434dd0eaf952e6a45088a
MIIKI <nakrob.s@ku.th>
2021-11-08 05:14:49 +0700

    Update updateFormStaffCheckpoint.php

--- views/staffcheckpoint/updateFormStaffCheckpoint.php
+++ views/staffcheckpoint/updateFormStaffCheckpoint.php
@@ -1,11 +1,10 @@
 <form method="get" action="">
+<label>ชื่อเจ้าหน้าที่ <input type="text" name="cname"
+        value="<?php echo $staffcheckpoint->cname;?>"/></label><br>
 
-    <label>รหัสเจ้าหน้าที่ <input type="text" name="staff_c_id"
-        value="<?php echo $staffcheckpoint->staffid;?>"/></label><br>
-    <label>ชื่อเจ้าหน้าที่ <input type="text" name="cname"
-        value="<?php echo $staffcheckpoint->staffname;?>"/></label><br>
+<input type="hidden"name="controller"value="staffcheckpoint"/>
+<input type="hidden" name="staff_c_id" value="<?php echo $staffcheckpoint->staff_c_id; ?>"/>
+<button type= "submit"name="action"value="index">back</button>
+<button type= "submit"name="action"value="updateStaffCheckpoint">update</button>
 
-    <input type="hidden" name="controller" value="staffcheckpoint"/>
-    <button type="submit" name="action" value="updateStaffCheckpoint">update</button>
-    <button type="submit" name="action" value="index">Back</button>
 </form>
