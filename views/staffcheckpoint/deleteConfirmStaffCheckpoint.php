f13d17e732a8d4d6aaa1660e2029111fde1f342c
MIIKI <nakrob.s@ku.th>
2021-11-08 05:14:44 +0700

    Update deleteConfirmStaffCheckpoint.php

--- views/staffcheckpoint/deleteConfirmStaffCheckpoint.php
+++ views/staffcheckpoint/deleteConfirmStaffCheckpoint.php
@@ -1,10 +1,10 @@
-<?php echo "<br>Are you sure to delete this staffcheckpoint? <br>
-            <br>รหัสเจ้าหน้าที่:$staffcheckpoint->staffid
-            <br>ชื่อเจ้าหน้าที่:$staffcheckpoint->staffname<br>";?>
-<form method="get" action="">
+<?php echo "<br>Are you sure to delete this staffcheckpoint<br>";
+        echo "รหัสเจ้าหน้าที่ : $staffcheckpoint->staff_c_id <br>";
+        echo "ชื่อเจ้าหน้าที่ : $staffcheckpoint->cname <br>";
+?>
+    <form method="get" action="">
     <input type="hidden" name="controller" value="staffcheckpoint"/>
-    <input type="hidden" name="staff_c_id" value="<?php echo $staffcheckpoint->staffid;?>"/>
-    <input type="hidden" name="cname" value="<?php echo $staffcheckpoint->staffname;?>"/>
-    <button type="submit" name="action" value="deleteStaffCheckpoint">Delete</button>
-    <button type="submit" name="action" value="index">Back</button>
-</form>
+    <input type="hidden" name="staff_c_id" value="<?php echo $staffcheckpoint->staff_c_id;?>"/>
+    <button type="submit" name="action" value="index">back</button>
+    <button type="submit" name="action" value="deleteStaffCheckpoint">delete</button>
+    </form>
