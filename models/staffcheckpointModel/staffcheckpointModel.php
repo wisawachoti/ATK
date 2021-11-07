f3fc0b69b543973b3d67fc8b051c5de39e709c31
MIIKI <nakrob.s@ku.th>
2021-11-08 05:14:42 +0700

    Update staffcheckpointModel.php

--- models/staffcheckpointModel/staffcheckpointModel.php
+++ models/staffcheckpointModel/staffcheckpointModel.php
@@ -1,89 +1,94 @@
 <?php
-class StaffCheckpoint{
-    public $staffid , $staffname;
-    public function __construct($staffid , $staffname)
+    class StaffCheckpoint{
+    public $staff_c_id,$cname;
+
+    public function __construct($staff_c_id,$cname)
     {
-        $this->staffid = $staffid;
-        $this->staffname = $staffname;
+        $this->staff_c_id = $staff_c_id;
+        $this->cname = $cname;
+
+
     }
+
     public static function getAll()
     {
-        $staffcheckpointList=[];
+        $staffcheckpointList = [];
         require("connection_connect.php");
-        $sql="SELECT staff_c_id , cname FROM StaffCheckpoint
-        ORDER BY staff_c_id";
-        $result =$conn->query($sql);
-        while($my_row=$result->fetch_assoc())
+        $sql = " SELECT * FROM StaffCheckpoint ";
+        $result = $conn->query($sql);
+         while($my_row = $result->fetch_assoc())
         {
-            $staffid=$my_row[staff_c_id];
-            $staffname=$my_row[cname];
-            $staffcheckpointList[]=new StaffCheckpoint($staffid,$staffname);
+         $staff_c_id = $my_row['staff_c_id'];
+         $cname = $my_row['cname'];
+         $staffcheckpointList[] = new StaffCheckpoint($staff_c_id,$cname);
         }
         require("connection_close.php");
         return $staffcheckpointList;
     }
 
-    public static function Add($staffid,$staffname)
+
+    public static function Add($staff_c_id,$cname)
     {
         require("connection_connect.php");
-        $sql="insert into StaffCheckpoint(staff_c_id,cname)values
-        ('$staffid','$staffname')";
-        $result =$conn->query($sql);
+        $sql = "INSERT INTO StaffCheckpoint(staff_c_id,cname)values
+        ('$staff_c_id','$cname')";
+        $result = $conn->query($sql);
+
         require("connection_close.php");
         return "add success $result rows";
     }
 
     public static function search($key)
     {
-        $staffcheckpointList=[];
+        $staffcheckpointList = [];
         require("connection_connect.php");
-        $sql = "SELECT staff_c_id , cname
-        FROM StaffCheckpoint
-        WHERE(staff_c_id like '%$key%' or cname like '%$key%')";
+        $sql="SELECT * FROM StaffCheckpoint
+        WHERE staff_c_id like '%$key%' or cname like '%$key%'";
         $result=$conn->query($sql);
         while($my_row=$result->fetch_assoc())
         {
-            $staffid=$my_row[staff_c_id];
-            $staffname=$my_row[cname];
-            $staffcheckpointList[]=new StaffCheckpoint($staffid,$staffname);
+            $staff_c_id = $my_row['staff_c_id'];
+            $cname = $my_row['cname'];
+            $staffcheckpointList[] = new StaffCheckpoint($staff_c_id,$cname);
         }
-        require("connection_close.php");
-        return $staffcheckpointList;
+            require("connection_close.php");
+            return $staffcheckpointList;
 
     }
-
-    public static function get($staffid,$staffname)
+    public static function get($staff_c_id)
     {
         require("connection_connect.php");
-        $sql = "SELECT staff_c_id,cname
-        FROM StaffCheckpoint
-        WHERE(staff_c_id = '$staffid' AND cname = '$staffname')
-        ORDER BY staff_c_id";
+        $sql="SELECT * FROM StaffCheckpoint
+        WHERE `staff_c_id` = '$staff_c_id'";
         $result=$conn->query($sql);
         $my_row=$result->fetch_assoc();
-        $staffid=$my_row[staff_c_id];
-        $staffname=$my_row[cname];
-
-        return new StaffCheckpoint($staffid,$staffname);
+        $staff_c_id = $my_row['staff_c_id'];
+        $cname = $my_row['cname'];
+        require("connection_close.php");
+        return new StaffCheckpoint($staff_c_id,$cname);
     }
 
-    public static function update($staffid,$staffname)
+    public static function update($staff_c_id,$cname)
     {
         require("connection_connect.php");
-        $sql="UPDATE StaffCheckpoint SET cname='$staffname'
-        WHERE  staff_c_id='$staffid'";
+        $sql="UPDATE StaffCheckpoint SET `cname`='$cname'
+        WHERE `staff_c_id`= '$staff_c_id' ";
         $result=$conn->query($sql);
         require("connection_close.php");
         return "update success $result row";
     }
 
-    public static function delete($staffid,$staffname)
+    public static function delete($staff_c_id)
     {
-        require("connection_connect.php");
-        $sql="DELETE FROM StaffCheckpoint WHERE staff_c_id='$staffid' AND cname='$staffname'";
-        $result=$conn->query($sql);
-        require("connection_close.php");
-        return "delete success $result row";
+         require("connection_connect.php");
+         $sql="DELETE FROM StaffCheckpoint WHERE staff_c_id='$staff_c_id'";
+         $result=$conn->query($sql);
+         require("connection_close.php");
+         return "delete success $result row";
     }
+
+
+
+
 }
 ?>
