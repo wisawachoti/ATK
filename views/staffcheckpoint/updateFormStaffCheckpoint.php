ae29b799264c33b7a23085ad93e0e38a8f0addcc
MIIKI <nakrob.s@ku.th>
2021-11-08 06:18:44 +0700

    Update updateFormStaffCheckpoint.php

--- views/staffcheckpoint/updateFormStaffCheckpoint.php
+++ views/staffcheckpoint/updateFormStaffCheckpoint.php
@@ -4,7 +4,8 @@
 
 <input type="hidden"name="controller"value="staffcheckpoint"/>
 <input type="hidden" name="staff_c_id" value="<?php echo $staffcheckpoint->staff_c_id; ?>"/>
-<button type= "submit"name="action"value="index">back</button>
 <button type= "submit"name="action"value="updateStaffCheckpoint">update</button>
+<button type= "submit"name="action"value="index">back</button>
+
 
 </form>
