32f126a38c387edb0fd3fc4864948c915b9415e5
MIIKI <nakrob.s@ku.th>
2021-11-09 01:33:30 +0700

    Update workscheduleModel.php

--- models/workscheduleModel/workscheduleModel.php
+++ models/workscheduleModel/workscheduleModel.php
@@ -26,14 +26,14 @@
             $result=$conn->query($sql);
             while($my_row=$result->fetch_assoc())
         {
-            $wsid=$my_row[ws_id];
-            $date=$my_row[date];
-            $staffid=$my_row[staff_c_id];
-            $staffname=$my_row[cname];
-            $staffpid=$my_row[staff_pc_id];
-            $staffpname=$my_row[name];
-            $stationid=$my_row[station_id];
-            $stationname=$my_row[s_name];
+            $wsid=$my_row['ws_id'];
+            $date=$my_row['date'];
+            $staffid=$my_row['staff_c_id'];
+            $staffname=$my_row['cname'];
+            $staffpid=$my_row['staff_pc_id'];
+            $staffpname=$my_row['name'];
+            $stationid=$my_row['station_id'];
+            $stationname=$my_row['s_name'];
             $wsList[]=new wsModel($wsid , $date , $staffid , $staffname , $staffpid , $staffpname , $stationid , $stationname);
         }
         require("connection_close.php");
@@ -59,24 +59,67 @@
             INNER JOIN StaffCheckpoint as sc ON sc.staff_c_id = ws.staff_c_id
             INNER JOIN StaffPositionCheckpoint as spc ON spc.staff_pc_id = ws.staff_pc_id
             INNER JOIN station ON station.station_id = ws.station_id
-            WHERE(ws.ws_id like '%$key%' or ws.date like '%$key%' or sc.staff_c_id like '%$key%' or sc.cname like '%$key%' or spc.staff_pc_id like '%$key%' or spc.name like '%$key%' or station.station_id like '%$key%' or station.s_name like '%$key%')
+            WHERE (ws.ws_id like '%$key%' or ws.date like '%$key%' or sc.staff_c_id like '%$key%' or sc.cname like '%$key%' or spc.staff_pc_id like '%$key%' or spc.name like '%$key%' or station.station_id like '%$key%' or station.s_name like '%$key%')
             ORDER BY ws.ws_id";
         $result=$conn->query($sql);
         while($my_row=$result->fetch_assoc())
         {
-            $wsid=$my_row[ws_id];
-            $date=$my_row[date];
-            $staffid=$my_row[staff_c_id];
-            $staffname=$my_row[cname];
-            $staffpid=$my_row[staff_pc_id];
-            $staffpname=$my_row[name];
-            $stationid=$my_row[station_id];
-            $stationname=$my_row[s_name];
+            $wsid=$my_row['ws_id'];
+            $date=$my_row['date'];
+            $staffid=$my_row['staff_c_id'];
+            $staffname=$my_row['cname'];
+            $staffpid=$my_row['staff_pc_id'];
+            $staffpname=$my_row['name'];
+            $stationid=$my_row['station_id'];
+            $stationname=$my_row['s_name'];
             $wsList[]=new wsModel($wsid , $date , $staffid , $staffname , $staffpid , $staffpname , $stationid , $stationname);
         }
         require("connection_close.php");
         return $wsList;
 
     }
+
+    public static function get($wsid ,$date , $staffname , $staffpname , $stationname)
+    {
+        require("connection_connect.php");
+        $sql="SELECT ws.date as d , sc.cname as cname , spc.name as n , station.s_name as  s_name
+                FROM WorkSchedule as ws
+                INNER JOIN StaffCheckpoint as sc ON sc.staff_c_id = ws.staff_c_id
+                INNER JOIN StaffPositionCheckpoint as spc ON spc.staff_pc_id = ws.staff_pc_id
+                INNER JOIN station ON station.station_id = ws.station_id
+                WHERE ws.ws_id = '$date'
+                ORDER BY ws.ws_id";
+        $result=$conn->query($sql);
+        $my_row=$result->fetch_assoc();
+        $date = $my_row['d'];
+        $staffname = $my_row['cname'];
+        $staffpname = $my_row['n'];
+        $stationname = $my_row['s_name'];
+        require("connection_close.php");
+        return new wsModel($wsid , $date , $staffid , $staffname , $staffpid , $staffpname , $stationid , $stationname);
+    }
+
+    public static function update($wsid, $date , $staffname , $staffpname , $stationname)
+    {
+        require("connection_connect.php");
+        $sql="UPDATE WorkSchedule SET date ='$date' , staff_c_id = '$staffname' , staff_pc_id = '$staffpname' , station_id = '$stationname'
+        WHERE ws_id= '$wsid'";
+        $result=$conn->query($sql);
+        require("connection_close.php");
+        return "update success $result row";
+    }
+
+    public static function delete($wsid)
+    {
+         require("connection_connect.php");
+         $sql="DELETE FROM WorkSchedule WHERE ws_id='$wsid'";
+         $result=$conn->query($sql);
+         require("connection_close.php");
+         return "delete success $result row";
+    }
+
+
+
+
 }
 ?>
