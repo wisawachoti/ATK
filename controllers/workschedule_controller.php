7a68bf7bc40f721b3812514e32e938bc450154ec
MIIKI <nakrob.s@ku.th>
2021-11-09 01:33:20 +0700

    Update workschedule_controller.php

--- controllers/workschedule_controller.php
+++ controllers/workschedule_controller.php
@@ -34,5 +34,56 @@ class wsController{
         $wsList=wsModel::search($key);
         require_once('views/workschedule/index_workschedule.php');
     }
+
+    public function updateFormWorkSchedule()
+    {
+        $wsid = $_GET['ws_id'];
+        $date = $_GET['date'];
+        $staffname = $_GET['cname'];
+        $staffpname = $_GET['name'];
+        $stationname = $_GET['s_name'];
+        $staffid = $_GET['staffid'];
+        $wsList=wsModel::getAll();
+        $staffcheckpointList = StaffCheckpoint::getall();
+        $staffpositioncheckpointList = StaffPositionCheckpoint::getAll();
+        $stationList = Station::getStation();
+        $workschedule = wsModel::get($wsid , $date,$staffname , $staffpname ,$stationname);
+        require_once('views/workschedule/updateFormWorkSchedule.php');
+    }
+
+    public function updateWorkSchedule()
+    {
+        $wsid = $_GET['ws_id'];
+        $date = $_GET['date'];
+        $staffname = $_GET['cname'];
+        $staffpname = $_GET['name'];
+        $stationname = $_GET['s_name'];
+        wsModel::update($wsid , $date,$staffname , $staffpname ,$stationname);
+        wsController::index();
+    }
+
+    public function deleteConfirmWorkSchedule()
+    {
+        $wsid = $_GET['ws_id'];
+        $date = $_GET['date'];
+        $staffname = $_GET['cname'];
+        $staffpname = $_GET['name'];
+        $stationname = $_GET['s_name'];
+        $staffid = $_GET['staffid'];
+        $workschedule = wsModel::get($wsid , $date,$staffname , $staffpname ,$stationname);
+        require_once('views/workschedule/deleteConfirmWorkSchedule.php');
+    }
+    public function deleteWorkSchedule()
+    {
+        $wsid=$_GET['ws_id'];
+        wsModel::delete($wsid);
+        wsController::index();
+    }
+
+
+
+
+
+
 }
 ?>
