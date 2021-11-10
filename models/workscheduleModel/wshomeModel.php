1dc29d90cba1cdd9d622d46cc576f81072f84ef4
MIIKI <nakrob.s@ku.th>
2021-11-11 02:38:27 +0700

    Update wshomeModel.php

--- models/workscheduleModel/wshomeModel.php
+++ models/workscheduleModel/wshomeModel.php
@@ -46,6 +46,38 @@
         return $wsList;
     }
 
+    public static function getStaff()
+    {
+        $wsList=[];
+        require("connection_connect.php");
+        $sql2 = "SELECT  MAX(ws.date) as date2
+                FROM WorkSchedule as ws ";
+                $result2=$conn->query($sql2);
+                $my_row2=$result2->fetch_assoc();
+                $date2 = $my_row2['date2'];
+        $sql = "SELECT ws.ws_id as num1 ,ws.date as num2 , StaffPositionCheckpoint.name as name , COUNT(sp.staff_c_id) as num3
+                from WorkSchedule as ws
+                INNER JOIN StaffCheckpoint as sp ON sp.staff_c_id = ws.staff_c_id
+                INNER JOIN StaffPositionCheckpoint on StaffPositionCheckpoint.staff_pc_id = ws.staff_pc_id
+                WHERE ws.date = '$date2'
+                GROUP BY ws.ws_id , StaffPositionCheckpoint.name , ws.date";
+            $result=$conn->query($sql);
+            while($my_row=$result->fetch_assoc())
+        {
+            $wsid=$my_row['num1'];
+            $date=$my_row['num2'];
+            $staffid=$my_row['num3'];
+            $staffname=$my_row['num3'];
+            $staffpid=$my_row['num3'];
+            $staffpname=$my_row['name'];
+            $stationid=$my_row['num3'];
+            $stationname=$my_row['num3'];
+            $wsList[]=new wshomeModel($wsid , $date , $staffid , $staffname , $staffpid , $staffpname , $stationid , $stationname);
+        }
+        require("connection_close.php");
+        return $wsList;
+    }
+
 
 
 }
