441334f7e7fb994375d68f667386f1eec72f7c2b
PakkawatB <91787198+PakkawatB@users.noreply.github.com>
2021-11-10 14:55:18 +0700

    Create connection_connect.php

---
+++ connection_connect.php
@@ -0,0 +1,18 @@
+<?php 
+$servername = "localHost";
+$username = "db21_100";
+$password = "db21_100";
+$dbname = "db21_100";
+$conn = new mysqli($servername,$username,$password);
+mysqli_set_charset($conn,"utf8");
+
+if($conn->connect_error)
+{
+    die("Connection failed: ".$conn->connect_error);
+}
+if(!$conn->select_db($dbname))
+{
+    die("Connection failed: ".$conn->connect_error);
+}
+
+?>
