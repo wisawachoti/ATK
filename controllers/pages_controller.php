7d76f57025a5fba14f52299539f2a3cb5b018202
PakkawatB <91787198+PakkawatB@users.noreply.github.com>
2021-11-10 14:55:13 +0700

    Create pages_controller.php

---
+++ controllers/pages_controller.php
@@ -0,0 +1,18 @@
+<?php
+
+    class pagescontroller
+    {
+        public function home()
+        {
+            require_once('views/pages/home.php');
+        }
+        public function error()
+        {
+            require_once('views/pages/error.php');
+        }
+    }
+
+
+
+
+?>
